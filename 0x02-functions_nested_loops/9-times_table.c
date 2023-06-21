e721e49d6203d94c9ef12fb57a39543c9ad2943d
Glandayzer <sadekmohamed979@gmail.com>
2023-06-21 15:27:48 -0700

    hard task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,41 @@
 #include "main.h"
+
 /**
  * times_table - function that prints time table
  * Return: void.
  */
 
 void times_table(void)
+
 {
-	int first_number = 0;
-	int second_number = 0;
-	int condition1, condition2, condition3;
-	int num1, num2;
 
-	for (first_number = 0; first_number <= 9; first_number++)
+	int x, y, z, u, d;
+
+	for (x = 0; x <= 9; x++)
 	{
-		for (second_number = 0; second_number <= 9; second_number++)
-		{
-			printf("%d", first_number * second_number);
-			fflush(stdout);
-			num1 = first_number * (second_number + 1);
-			num2 = (first_number * (second_number + 1)) % 10;
-			condition2 = (int)(num1 == num2);
-			num1 = first_number * second_number;
-			condition1 = (int)(num1 == num1 % 10);
-			condition3 = (int)(second_number != 9);
-
-			if (condition1 && condition2 && condition3)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				continue;
-			}
-
-			if (condition3)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-
-		_putchar('\n');
+	for (y = 0; y <= 9; y++)
+	{
+	z = x * y;
+	if (z > 9)
+	{
+	u = z % 10;
+	d = (z - u) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	_putchar(u + '0');
+	}
+	else
+	{
+	if (y != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(z + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
