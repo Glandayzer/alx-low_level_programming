89df74fd8aa97153e5a93c2bea999c9526c17ff9
Glandayzer <sadekmohamed979@gmail.com>
2023-07-06 14:03:31 -0700

    tasks

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#include <unistd.h>
+
+void _puts_recursion(char *s);
+int  _putchar(char c);
+void _print_rev_recursion(char *s);
+int  _strlen_recursion(char *s);
+int  factorial(int n);
+int  _pow_recursion(int x, int y);
+int  _sqrt_recursion(int n);
+int  _find_sqrt(int n, int i);
+int  is_prime_number(int n);
+int  _is_p(int n, int i);
+int  is_palindrome(char *s);
+int check_palindrome(char *s, int left, int right);
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
